ac5022cc3b057d8bb80942c33e259db9c8392b5a
Peter Johnson <peterandersjohnson@outlook.com>
2026-02-21 17:43:46 -0600

    Add on stuff not clear at all this is necessary

---
+++ bin/main/dendryterra/Benchmark.ps1
@@ -0,0 +1,11 @@
+#Build:
+
+#cd /c/Projects/DendryTerra && JAVA_HOME="C:/JAVA/jdk-23" ./gradlew build 2>&1
+
+#Benchmark:
+
+cd "c:/Projects/DendryTerra"
+$Env:JAVA_HOME = 'C:/JAVA/jdk-23'
+#C:/JAVA/jdk-23/bin/java.exe -cp DendryTerra.jar dendryterra.DendryBenchmarkRunner 32 2>&1
+
+./gradlew benchmark --args="16" 2>&1
