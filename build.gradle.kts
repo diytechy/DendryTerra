8ac9c20f97355e8ba0811f9d3ff288eab3b4a363
Peter Johnson <peterandersjohnson@outlook.com>
2026-02-22 18:25:05 -0600

    Build using available repsy addon

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,18 @@ repositories {
         name = "Solo Studios"
         url = uri("https://maven.solo-studios.ca/releases")
     }
+    maven {
+        name = "Repsy-Terra"
+        url = uri("https://repo.repsy.io/mvn/diytechy/terra")
+    }
 }
 
 // For addon compilation (provided by Terra at runtime)
 dependencies {
-    compileOnly("com.dfsek.terra:manifest-addon-loader:1.0.0-BETA+a159debe3")
+    compileOnly("com.dfsek.terra:manifest-addon-loader:1.0.0-BETA-ec788bf")
     compileOnly("com.dfsek:seismic:0.8.2")
-    compileOnly("com.dfsek.terra:base:7.0.0-BETA+a159debe3")
+    compileOnly("com.dfsek.terra:base:7.0.0-BETA-ec788bf")
+    compileOnly("com.dfsek.terra:config-noise-function:1.2.0-BETA-ec788bf")
     compileOnly("com.dfsek.tectonic:common:4.2.1")
     compileOnly("org.slf4j:slf4j-api:2.0.9")
     compileOnly("com.github.ben-manes.caffeine:caffeine:3.1.8")
